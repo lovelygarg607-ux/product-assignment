51f097302932d7c9b41e74d350f4de52a3bf5a80
Lovely-dev 687 <lgarg4812@gmail.com>
2026-01-03 20:59:30 +0530

    Fix routing: redirect root to home

--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
-import Homepage from "./Components/Homepage"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate
+} from "react-router-dom";
 
+import Homepage from "./Components/Homepage";
 import EditProduct from './Components/EditProduct';
-
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='/home' element={<Homepage />} />
-      <Route path='/editproduct/:id' element={<EditProduct />} />
 
+      {/* Redirect / → /home */}
+      <Route index element={<Navigate to="/home" />} />
 
-
-
-
+      <Route path='home' element={<Homepage />} />
+      <Route path='editproduct/:id' element={<EditProduct />} />
 
     </Route>
   )
-)
-
-
-
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
